8918f6cd7fb0d54ff8f5ecdc9395f343e424da2e
jwcen <jvvcen@gmail.com>
2023-11-13 14:36:17 +0800

    Java SpringBoot-init Project model

---
+++ src/main/java/com/example/springbootinit/esdao/PostEsDao.java
@@ -0,0 +1,16 @@
+package com.example.springbootinit.esdao;
+
+import com.example.springbootinit.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+ * @author <a href="https://github.com/jwcen">Jcen</a>
+ * 
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/main/java/com/example/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/jwcen">Jcen</a>
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
